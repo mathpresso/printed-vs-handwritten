851a35cba7d353feaba34a5f84939ac8087a06f5
nutella <nutella@nutellas-MacBook-Pro.local>
2020-03-06 11:34:41 +0900

    Added validation.zip

--- classify.py
+++ classify.py
@@ -7,13 +7,11 @@ from PIL import Image
 sys.path.append("../ocrd_typegroups_classifier")
 from ocrd_typegroups_classifier.typegroups_classifier import TypegroupsClassifier
 
-if len(sys.argv)!=2:
-    print('Syntax: python3 %s input-textline.jpg' % sys.argv[0])
 
 img = Image.open(input())
 tgc = TypegroupsClassifier.load(os.path.join('ocrd_typegroups_classifier', 'models', 'classifier.tgc'))
 
-result = tgc.classify(img, 75, 64, False)
+result = tgc.classify(img, 150, 12, False)
 esum = 0
 for key in result:
     esum += exp(result[key])
--- train-model.py
+++ train-model.py
@@ -20,11 +20,11 @@ from ocrd_typegroups_classifier.network.densenet import densenet121
 
 # Loading and preparing the network
 net = densenet121(num_classes=2)
-#net = resnet18(num_classes=12)
+# net = resnet18(num_classes=12)
 
 # Some settings for the training
 learning_rate = 0.1
-#weight_decay = 0.0001
+# weight_decay = 0.0001
 weight_decay = 0
 lr_decay = lambda epoch: 0.97 ** epoch
 reconstruction_loss = nn.MSELoss()
@@ -35,21 +35,21 @@ scheduler = LambdaLR(optimizer, lr_lambda=[lr_decay])
 # Creation of the typegroup classifier
 tgc = TypegroupsClassifier(
     {
-        'handwritten':0,
-        'printed':1
+        'handwritten': 0,
+        'printed': 1
     },
     net
 )
-if os.path.exists(os.path.join('ocrd_typegroups_classifier', 'models', 'classifier.tgc')):
-    tgc = TypegroupsClassifier.load(os.path.join('ocrd_typegroups_classifier', 'models', 'classifier.tgc'))
+if os.path.exists(os.path.join('ocrd_typegroups_classifier', 'models', 'classifier-last.tgc')):
+    tgc = TypegroupsClassifier.load(os.path.join('ocrd_typegroups_classifier', 'models', 'classifier-last.tgc'))
 
 # Data transformation & loading
 # Note that due to the rotation, having several sequential shearing
 # transforms sequentially is not the same as having only one with
 # a larger range.
 trans = transforms.Compose([
-    transforms.RandomAffine(180),
-    #transforms.RandomCrop(224),
+    transforms.RandomAffine(180, shear=3),
+    # transforms.RandomCrop(224),
     # transforms.RandomResizedCrop(150, scale=(0.25, 1.0), ratio=(0.9, 1.11), interpolation=2),
     transforms.RandomResizedCrop(150, scale=(0.9, 1.0), ratio=(0.9, 1.11), interpolation=2),
     # transforms.Resize(150),
@@ -77,10 +77,10 @@ data_loader = torch.utils.data.DataLoader(training,
 
 # Iterating over the data
 print('Starting the training - grab a coffee and a good book!')
-for epoch in range(100):
+for epoch in range(1000):
     # Modify learning rate
     scheduler.step()
-    
+
     # Iterate over the data
     lossSum = 0
     good = 0
@@ -89,11 +89,11 @@ for epoch in range(100):
     for sample, label in tqdm(data_loader, desc='Training'):
         # Move data to device
         sample = sample.to(tgc.dev)
-        label  = label.to(tgc.dev)
-        
+        label = label.to(tgc.dev)
+
         # Training the classifier on samples with known labels
         sample, label = tgc.filter(sample, label)
-        if len(label)==0: # no known labels
+        if len(label) == 0:  # no known labels
             continue
         out = tgc.network(sample)
         closs = classification_loss(out, label)
@@ -101,47 +101,45 @@ for epoch in range(100):
         closs.backward()
         optimizer.step()
         lossSum += closs.item()
-        
+
         # Computing accuracy
         _, p = torch.max(out, 1)
-        good += torch.sum(p==label).item()
+        good += torch.sum(p == label).item()
         known += len(label)
-    print('Epoch %d, loss %.1f, %d/%d=%.1f%%' % (epoch, lossSum, good, known, (100.0*good)/known))
-    
+    print('Epoch %d, loss %.1f, %d/%d=%.1f%%' % (epoch, lossSum, good, known, (100.0 * good) / known))
+
     targets = list()
     results = list()
     good = 0
-    bad  = 0
+    bad = 0
     with torch.no_grad():
         tgc.network.eval()
         for idx in tqdm(range(validation.__len__()), desc='Evaluation'):
             sample, target = validation.__getitem__(idx)
             path, _ = validation.samples[idx]
-            if target==-1:
+            if target == -1:
                 continue
             result = tgc.classify(sample, 150, 12, True)
             highscore = max(result)
             label = tgc.classMap.cl2id[result[highscore]]
             targets.append(target)
             results.append(label)
-            if target==label:
+            if target == label:
                 good += 1
             else:
                 bad += 1
     with open('results.dat', 'wb') as f:
         pickle.dump(targets, f)
         pickle.dump(results, f)
-    
-    accuracy = 100*good/float(good+bad)
-    
+
+    accuracy = 100 * good / float(good + bad)
+
     print('    Good:', good)
     print('     Bad:', bad)
     print('Accuracy:', accuracy)
-    
-    if accuracy>best_validation:
+
+    if accuracy > best_validation:
         tgc.save(os.path.join('ocrd_typegroups_classifier', 'models', 'classifier.tgc'))
         best_validation = accuracy
         print('Network saved')
-    else:
-        tgc.save(os.path.join('ocrd_typegroups_classifier', 'models', 'classifier-last.tgc'))
-
+    tgc.save(os.path.join('ocrd_typegroups_classifier', 'models', 'classifier-last.tgc'))
